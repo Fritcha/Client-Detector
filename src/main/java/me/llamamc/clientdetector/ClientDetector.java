ec699981e84497ece35b64cb825a3f7e59363144
Llama-MC <123831214+Fritcha@users.noreply.github.com>
2023-07-14 19:48:30 +0200

    Add files via upload

--- src/main/java/me/llamamc/clientdetector/ClientDetector.java
+++ src/main/java/me/llamamc/clientdetector/ClientDetector.java
@@ -1,14 +1,37 @@
 package me.llamamc.clientdetector;
 
 import me.llamamc.clientdetector.commands.ClientCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ClientDetector extends JavaPlugin {
+public final class ClientDetector extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getLogger().info("Client Detector loaded successfully!");
+        saveDefaultConfig();
         getCommand("client").setExecutor(new ClientCommand());
+        Bukkit.getPluginManager().registerEvents(this, this);
+        getLogger().info("Client Detector loaded successfully!");
 
+    }
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if(getConfig().getBoolean("IncludeClientInJoinMessage")) {
+            event.setJoinMessage("");
+            Bukkit.getScheduler().runTaskLater(this, () -> {
+                Bukkit.broadcastMessage("§e" + event.getPlayer().getDisplayName() + " joined the game using " + event.getPlayer().getClientBrandName());
+
+            }, 2L);
+        }
+        Bukkit.getScheduler().runTaskLater(this, () -> {
+            if(getConfig().getStringList("BlockedClients").contains(event.getPlayer().getClientBrandName())) {
+                event.getPlayer().kickPlayer("§cClient: " + "\"" + event.getPlayer().getClientBrandName() + "\" is not allowed on this server!");
+
+            }
+        }, 2L);
     }
 }
